c30d81ef63b16c6c5db6cce4eadb7994a816231f
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-06-11 20:19:36 +0530

    Updated contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export default function Contact() {
         },
     ]
     return (
-        <div id='contact' className='bg-[#243d61] py-10'>
+        <div id='contact' className='min-h-screen bg-[#243d61] py-10'>
             <div className='text-4xl text-center text-[#00ffff] font-bold'>
                 Contact Me
             </div>
